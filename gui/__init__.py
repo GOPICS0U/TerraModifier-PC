aaf20d646eff421d5bac919ce36fac0876d48029
P1CS0U <clemofficiel91@gmail.com>
2025-06-05 16:09:16 +0200

    Create __init__.py

---
+++ gui/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module d'interface graphique pour TerraGenesis PC
+"""
+
+from .main_window import MainWindow
+from .planet_selection import PlanetSelectionDialog
+from .game_interface import GameInterface
+
+__all__ = ['MainWindow', 'PlanetSelectionDialog', 'GameInterface']
