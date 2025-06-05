f33e2d1387a4cd0b0c31e11fe7cb04d360945f42
P1CS0U <clemofficiel91@gmail.com>
2025-06-05 16:05:31 +0200

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+"""
+Module de configuration pour TerraGenesis PC
+"""
+
+from .settings import GameSettings
+from .constants import *
+
+__all__ = ['GameSettings', 'WINDOW_WIDTH', 'WINDOW_HEIGHT', 'GAME_TITLE']
