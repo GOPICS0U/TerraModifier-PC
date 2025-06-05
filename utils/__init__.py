cd1a7903a8b66f46a97fdfb5d03fd1d5fb9a48c0
P1CS0U <clemofficiel91@gmail.com>
2025-06-05 16:09:55 +0200

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module utilitaires pour TerraGenesis PC
+"""
+
+from .audio_manager import AudioManager
+from .save_manager import SaveManager
+from .helpers import *
+
+__all__ = ['AudioManager', 'SaveManager', 'format_time', 'format_number']
